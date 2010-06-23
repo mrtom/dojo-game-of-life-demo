d680704da774f172c53f535fbedb8ab1080fef1f
Tom Elliott <mrtom@cantab.net>
2010-06-23 17:59:33 +0100

    Updated the scripts for creating seeds from images (still not really finished, but workingish), and added seeds for the dojo toolkit/inverse. Added pointless bling to the readme

--- python/GenerateDojoLogoSeed.py
+++ python/GenerateDojoLogoSeed.py
@@ -1,5 +1,17 @@
 from PIL import Image
+import re
+
 im = Image.open("../resources/dojo_logo.bmp");
 (width, height) = im.size
 data = list(im.getdata())
-print list((x,y) for (pixel,(x,y)) in zip(data, ((x,y) for x in range(width) for y in range(height))) if pixel == 0)
+
+# Just print out in default python formatting
+print list((x,y) for (pixel,(x,y)) in zip(data, ((x,y) for x in range(height) for y in range(width))) if pixel == 1)
+
+# Attempt to print as JSON, but this doens't quite work
+#print list("{x:%i, y:%i}"%(x,y) for (pixel,(x,y)) in zip(data, ((x,y) for x in range(width) for y in range(height))) if pixel == 0)
+
+# Attempt to print as JSON by converting the output to a list and using a regex
+#coords = list((x,y) for (pixel,(x,y)) in zip(data, ((x,y) for x in range(width) for y in range(height))) if pixel == 0)
+#print type(''.join(coords))
+#re.sub("\((\d+), (\d+)\)", r"{x:\1, y:\2}", coords)
---
+++ python/perlRegex.sh
@@ -0,0 +1,1 @@
+ perl -pe 's/\((\d+), (\d+)\)/{x:$2, y:$1}/g' dojoLogoSeed.txt > dojoLogoSeed2.txt
